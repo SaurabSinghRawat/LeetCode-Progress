0af89e7c4336c06a07bc4290903083f182d95e44
Saurab Singh Rawat <saurab.s.rawat2024@gmail.com>
2025-04-02 18:05:27 +0530

    Time: 2 ms (97.57%) | Memory: 42 MB (20.35%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+class Solution {
+    public static boolean isValid(String s) {
+           Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char openBracket = stack.pop();
+                if (!isMatchingPair(openBracket, ch)) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    private static boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));        // true
+        System.out.println(isValid("()[]{}"));    // true
+        System.out.println(isValid("(]"));        // false
+        System.out.println(isValid("([])"));      // true
+    }
+}
