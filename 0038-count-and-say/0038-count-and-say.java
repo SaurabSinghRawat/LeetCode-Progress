f62ed8c54b1de8da2818eb9eef4ad39dbe5f3d3b
Saurab Singh Rawat <saurab.s.rawat2024@gmail.com>
2025-04-18 09:50:30 +0530

    Time: 6 ms (49.76%), Space: 41.2 MB (88.07%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+
+        String result = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder current = new StringBuilder();
+            int count = 1;
+            char prevChar = result.charAt(0);
+
+            for (int j = 1; j < result.length(); j++) {
+                char currentChar = result.charAt(j);
+                if (currentChar == prevChar) {
+                    count++;
+                } else {
+                    current.append(count).append(prevChar);
+                    prevChar = currentChar;
+                    count = 1;
+                }
+            }
+            current.append(count).append(prevChar);
+            result = current.toString();
+        }
+        return result;
+    }
+}
