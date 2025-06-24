70fb1ce577bf56249979f3ac9fcea1fdb88eecec
Saurab Singh Rawat <saurab.s.rawat2024@gmail.com>
2025-06-24 11:19:19 +0530

    Time: 5 ms (79.06%), Space: 44.4 MB (97.25%) - LeetHub

---
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        int n = nums.length;
+        boolean[] marked = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+                int start = Math.max(0, i - k);
+                int end = Math.min(n - 1, i + k);
+                for (int j = start; j <= end; j++) {
+                    marked[j] = true;
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (marked[i]) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
