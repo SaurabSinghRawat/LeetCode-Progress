8e6ef92d48a5aa53f02dacfed1ce2711b96fb1a2
Saurab Singh Rawat <saurab.s.rawat2024@gmail.com>
2025-07-28 06:19:11 +0530

    Time: 8 ms (80.38%), Space: 41 MB (72.92%) - LeetHub

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    int maxOr = 0, count = 0;
+    
+    public int countMaxOrSubsets(int[] nums) {
+        for (int num : nums) {
+            maxOr |= num;
+        }
+        dfs(nums, 0, 0);
+        return count;
+    }
+    
+    private void dfs(int[] nums, int index, int currentOr) {
+        if (index == nums.length) {
+            if (currentOr == maxOr) {
+                count++;
+            }
+            return;
+        }
+        // Include nums[index]
+        dfs(nums, index + 1, currentOr | nums[index]);
+        // Exclude nums[index]
+        dfs(nums, index + 1, currentOr);
+    }
+}
