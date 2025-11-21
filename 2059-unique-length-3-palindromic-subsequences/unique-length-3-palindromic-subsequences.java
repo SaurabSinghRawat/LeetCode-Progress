9d7cc53974a2d2002ab1db47ae747221a614aa64
Saurab Singh Rawat <saurab.s.rawat2024@gmail.com>
2025-11-21 07:15:37 +0530

    Time: 38 ms (95.44%) | Memory: 47.1 MB (13.68%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,32 +1,36 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
+        int n = s.length();
         int[] first = new int[26];
         int[] last = new int[26];
-        Arrays.fill(first, -1);
-        
-        for (int i = 0; i < s.length(); i++) {
-            int curr = s.charAt(i) - 'a';
-            if (first[curr] == - 1) {
-                first[curr] = i;
-            }
-            
-            last[curr] = i;
+
+        // initialize positions
+        java.util.Arrays.fill(first, -1);
+        java.util.Arrays.fill(last, -1);
+
+        // record first and last appearance of each character
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            if (first[c] == -1) first[c] = i;
+            last[c] = i;
         }
-        
-        int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (first[i] == -1) {
-                continue;
+
+        int count = 0;
+
+        // for each possible outer character
+        for (int c = 0; c < 26; c++) {
+            if (first[c] != -1 && last[c] != -1 && last[c] > first[c]) {
+                boolean[] seen = new boolean[26];
+                // scan middle characters strictly between first and last
+                for (int i = first[c] + 1; i < last[c]; i++) {
+                    seen[s.charAt(i) - 'a'] = true;
+                }
+                // count distinct middle characters
+                for (boolean b : seen) {
+                    if (b) count++;
+                }
             }
-            
-            Set<Character> between = new HashSet();
-            for (int j = first[i] + 1; j < last[i]; j++) {
-                between.add(s.charAt(j));
-            }
-            
-            ans += between.size();
         }
-        
-        return ans;
+        return count;
     }
 }
